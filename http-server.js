919cdc02ba4775c44fc31b0927aba1c213241a30
RimveBiceps <kanapka.rimvydas@gmail.com>
2021-03-06 16:58:46 +0200

    [ADD] Crediantials in web server file

--- http-server.js
+++ http-server.js
@@ -1,3 +1,8 @@
+/*
+Node.js Web Server for working with HTTP methods.
+Using JavaScript ES6 (ECMAScript 6) standard.
+Coded by Rimvydas Kanapka.
+*/
 import { createServer } from 'http';
 import { readFile } from 'fs';
 
@@ -5,6 +10,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer(function(req, res) {
+    console.log("Response status code:", res.statusCode);
     res.writeHead(200, {
         'Content-Type': 'text/html',
         'X-Powered-By': 'HTTP 1.1 Server'
